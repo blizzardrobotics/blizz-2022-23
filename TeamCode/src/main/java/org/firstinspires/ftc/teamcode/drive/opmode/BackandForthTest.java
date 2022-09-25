a79ec38e92017fb0f35a45ce0ea1e3e0b31f291c
iniyan-kannappan <iniyan.kannappan@gmail.com>
2022-09-24 18:35:55 -0700

    prints distance in back and forth

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/BackandForthTest.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.drive.opmode;
+
+public class BackandForthTest {
+    public static void main(String args[]){
+        System.out.println("Hello");
+        BackAndForth backandforth=new BackAndForth();
+        System.out.println("Distance"+backandforth.DISTANCE);
+    }
+
+}
